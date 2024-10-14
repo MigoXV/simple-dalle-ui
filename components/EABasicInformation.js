72bf1b79c231b6dabb17958561c41941b76bd29d
MigoXV <1695587905@qq.com>
2024-10-14 12:37:00 +0800

    basic information

---
+++ components/EABasicInformation.js
@@ -0,0 +1,12 @@
+import styles from "../styles/index.module.css";
+
+export default () => {
+    return (
+        <div>
+            <img src="/image-gen/LOGO2024.png" className={styles.icon} />
+            <h4>DALLE绘图</h4>
+            中南大学-电子设计爱好者协会<br />
+            <br />
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { useState } from "react";
 import styles from "../styles/index.module.css";
 import DalleForm from "../components/dalle_form";
+import EABasicInformation from "../components/EABasicInformation";
 import FrostedGlassLayoutWithBackground from "../components/dalle_background";
 import { ThemeProvider, GradientButton } from "@lobehub/ui";
 
@@ -12,32 +13,27 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
 
   return (
-    <ThemeProvider>
-      <FrostedGlassLayoutWithBackground>
-        <div>
-          <Head>
-            <title>Vectura.ai - simple DALL-E3 FrontEnd</title>
-            <link rel="icon" href="/image-gen/icon.png" />
-          </Head>
-          <main className={styles.main}>
 
-            <img src="/image-gen/LOGO2024.png" className={styles.icon} />
-            <h4>DALLE绘图</h4>
-            中南大学-电子设计爱好者协会<br />
-            <br />
-            <DalleForm setResult={setResult} setRevisedPrompt={setRevisedPrompt} loading={loading} setLoading={setLoading} />
-            {loading && <div><small>图片正在生成，请耐心等待</small></div>}
-            <a href={result} target="_blank" rel="noopener noreferrer">
-              {result && <img src={result} className={styles.result} />}
-            </a>
-            {result && revised_prompt &&
-              <div className={styles.result}>
-                <small>Revised prompt used to generate image:</small>
-                <p>{revised_prompt}</p>
-              </div>
-            }
-          </main>
-        </div>
+    <ThemeProvider>
+      <Head>
+        <title>Vectura.ai - simple DALL-E3 FrontEnd</title>
+        <link rel="icon" href="/image-gen/icon.png" />
+      </Head>
+      <FrostedGlassLayoutWithBackground>
+        <main className={styles.main}>
+          <EABasicInformation />
+          <DalleForm setResult={setResult} setRevisedPrompt={setRevisedPrompt} loading={loading} setLoading={setLoading} />
+          {loading && <div><small>图片正在生成，请耐心等待</small></div>}
+          <a href={result} target="_blank" rel="noopener noreferrer">
+            {result && <img src={result} className={styles.result} />}
+          </a>
+          {result && revised_prompt &&
+            <div className={styles.result}>
+              <small>Revised prompt used to generate image:</small>
+              <p>{revised_prompt}</p>
+            </div>
+          }
+        </main>
       </FrostedGlassLayoutWithBackground>
     </ThemeProvider>
   );
