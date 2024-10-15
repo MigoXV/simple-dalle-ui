4e8802147fe05ce9409abfbf79967a734ee2d56a
MigoXV <1695587905@qq.com>
2024-10-15 21:33:50 +0800

    ResultArea

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import Head from "next/head";
 import { useState } from "react";
 import styles from "../styles/index.module.css";
 import DalleForm from "../components/dalle_form";
+import ResultArea from "../components/ResultArea";
 import EABasicInformation from "../components/EABasicInformation";
 import FrostedGlassLayoutWithBackground from "../components/dalle_background";
-import { ThemeProvider, GradientButton } from "@lobehub/ui";
+import { ThemeProvider } from "@lobehub/ui";
 
 export default function Home() {
 
@@ -13,28 +14,20 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
 
   return (
-
-    <ThemeProvider>
+    <>
       <Head>
         <title>Vectura.ai - simple DALL-E3 FrontEnd</title>
         <link rel="icon" href="/image-gen/icon.png" />
       </Head>
-      <FrostedGlassLayoutWithBackground>
-        <main className={styles.main}>
-          <EABasicInformation />
-          <DalleForm setResult={setResult} setRevisedPrompt={setRevisedPrompt} loading={loading} setLoading={setLoading} />
-          {loading && <div><small>图片正在生成，请耐心等待</small></div>}
-          <a href={result} target="_blank" rel="noopener noreferrer">
-            {result && <img src={result} className={styles.result} />}
-          </a>
-          {result && revised_prompt &&
-            <div className={styles.result}>
-              <small>Revised prompt used to generate image:</small>
-              <p>{revised_prompt}</p>
-            </div>
-          }
-        </main>
-      </FrostedGlassLayoutWithBackground>
-    </ThemeProvider>
+      <ThemeProvider enableGlobalStyle={true} defaultThemeMode="light" enableWebfonts={true} customTheme={{ neutralColor: 'mauve', primaryColors: "orange" }}>
+        <FrostedGlassLayoutWithBackground>
+          <main className={styles.main}>
+            <EABasicInformation />
+            <DalleForm setResult={setResult} setRevisedPrompt={setRevisedPrompt} loading={loading} setLoading={setLoading} />
+            <ResultArea loading={loading} result={result} revised_prompt={revised_prompt} />
+          </main>
+        </FrostedGlassLayoutWithBackground>
+      </ThemeProvider>
+    </>
   );
 }
